bedd038036e4f0bc781e8a3dd898f502e3e8b742
Max Wheeler <max@makenosound.com>
2009-09-25 22:03:43 +1000

    Added `last` and `active` params output, now includes datasource for easier looping.

---
+++ data-sources/data.url_segments.php
@@ -0,0 +1,82 @@
+<?php
+
+  require_once(TOOLKIT . '/class.datasource.php');
+  
+  Class datasourceurl_segments extends Datasource{
+    
+    public $dsParamROOTELEMENT = 'url-segments';
+
+    public function __construct(&$parent, $env=NULL, $process_params=true)
+    {
+      parent::__construct($parent, $env, $process_params);
+      $this->_dependencies = array();
+    }
+    
+    public function about()
+    {
+      return array(
+           'name' => 'URL Segments as XML',
+           'author' => array(
+              'name' => 'Max Wheeler',
+              'website' => 'http://makenosound.com/'),
+           'version' => '1.0.1',
+           'release-date' => '2009-09-25');
+    }
+      
+    public function allowEditorToParse()
+    {
+      return false;
+    }
+    
+    public function grab(&$param_pool)
+      {
+      $result = new XMLElement($this->dsParamROOTELEMENT);
+      
+      foreach ($this->_env as $key => $value)
+      { 
+        switch ($key) {
+          case 'param':
+            foreach ($this->_env[$key] as $key => $value)
+            {
+              if (substr($key, 0, 12) == "url-segment-")
+              {
+                switch (substr($key, 12)) {
+                  case 'last':
+                    $result->setAttribute("last", $value);
+                    break;
+                  case 'active':
+                    $result->setAttribute("active", $value);
+                    break;
+                  default:
+                    $param = new XMLElement("segment", General::sanitize($value));
+                    $result->appendChild($param);
+                    break;
+                }
+              }
+            }
+            break;
+          case 'env':
+            foreach ($this->_env[$key]['pool'] as $key => $value)
+            {
+              if (substr($key, 0, 12) == "url-segment-")
+              {
+                switch (substr($key, 12)) {
+                  case 'last':
+                    $result->setAttribute("last", $value);
+                    break;
+                  case 'active':
+                    $result->setAttribute("active", $value);
+                    break;
+                  default:
+                    $param = new XMLElement("segment", General::sanitize($value));
+                    $result->appendChild($param);
+                    break;
+                }
+              }
+            }
+            break;
+        }
+      }
+      return $result;
+    }
+  }
--- extension.driver.php
+++ extension.driver.php
@@ -1,14 +1,14 @@
 <?php
   Class extension_url_segments extends Extension
-  {  
+  {
+    
+    /*-------------------------------------------------------------------------
+      Extension definition
+    -------------------------------------------------------------------------*/
     public function about()
     {
-
-      /*-------------------------------------------------------------------------
-        Extension definition
-      -------------------------------------------------------------------------*/
       return array('name' => 'URL Segments',
-                   'version' => '1.0.0',
+                   'version' => '1.0.1',
                    'release-date' => '2009-09-16',
                    'author' => array('name' => 'Max Wheeler',
                       'website' => 'http://makenosound.com/',
@@ -32,19 +32,33 @@
     # Adds URL segments to page parameters based on $current-page param
     public function segments_to_params($context)
     {
-      # Get current path from page params
+      # Get current path from page params, replacement for lack of "before_needle" in strstr
       $needle = "?";
       $current_path = $context['params']['current-path'];
-      if ( strpos($current_path, $needle) != 0) $current_path = substr(strrev(strstr(strrev($current_path), strrev($needle))), 0, -strlen($needle));
+      if ( strpos($current_path, $needle) != 0)
+      {
+        $current_path = substr(strrev(strstr(strrev($current_path), strrev($needle))), 0, -strlen($needle));
+      }
       $current_path = ltrim( $current_path, "/");
       $segments = split("/",  $current_path);
       
-      
-      # Add to context array
+      # Add to context['params'] array
       foreach ($segments as $key => $segment)
       {
-        if (empty($segment)) return;
+        if (empty($segment))
+        {
+          unset($segments[$key]);
+          continue;
+        }
         $context['params']['url-segment-' . ($key + 1)] = $segment;
+      }
+      # If no segment (i.e. index page)
+      if (count($segments) == 0) {
+        $context['params']['url-segment-last'] = $context['params']['url-segment-1'] = $context['params']['current-page'];
+        $context['params']['url-segment-active'] = 1;
+      } else {
+        $context['params']['url-segment-last'] = end($segments);
+        $context['params']['url-segment-active'] = count($segments); 
       }
     }
   }
